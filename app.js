a400a5e2fe2bc720b1c557d34b696b0f614d90f1
Sam Coe <samuel.m.coe@gmail.com>
2015-02-05 11:39:48 -0600

    Trying to get the login form submission to work for screen scrape

--- app.js
+++ app.js
@@ -5,24 +5,47 @@ var express = require('express')
 var prompt = require('prompt')
 var request = require('request')
 var cheerio = require('cheerio')
+var FormData = require('form-data')
 var app = express();
 var url;
 var css_el;
 
 app.get('/scrape', function(req, res){
 	prompt.start()
-	prompt.get(['url','css_el'], function(err, result){
-		url = result.url
+	prompt.get(['url','css_el', 'user', 'pass'], function(err, result){
 		css_el = result.css_el
-		request(url, function(err, res, body){
-			if(!err){
+		url = result.url
+
+		var form = new FormData();
+
+		var resource = result.user
+		var password = result.pass
+
+		form.append('resource', resource)
+		form.append('password', password)
+		console.log(form)
+
+		form.submit(url, function(err, res){
+			res.resume();
+		})
+
+		var r = request.post(url, function(err, res, body) {
+			if (err) {
+				return console.error('upload failed: ', err)
+			}
+			request(url, function(err, res, body){
+				if(err){
+					callback.call(null, new Error('Login failed'))
+					return;
+				}
+				console.log(body)
 				var $ = cheerio.load(body)
 				$('select').filter(function(){
 					var data = $(this)
 					var job = data.children().first().text()
 					console.log(job)
 				})
-			}
+			})
 		})
 	})
 })
